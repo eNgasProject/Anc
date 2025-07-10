29f9c58c8979321740ce0b56712107cb157333bf
eNgasProject <pinzinlimited@gmail.com>
2025-07-10 14:30:34 +0000

    Update app.js

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ function initDOMElements() {
 // Load vocabulary data from JSON file
 async function loadData() {
   try {
-    const response = await fetch('https://ngasapp.netlify.app/data.json');
+    const response = await fetch('https://engasproject.github.io/Anc/data.json');
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
